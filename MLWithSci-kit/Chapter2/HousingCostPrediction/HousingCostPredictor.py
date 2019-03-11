40b3b18e56f0921ccec4eac635dac4bbc966760d
DrN3RD <hugo.danielson@hotmail.com>
2019-03-11 14:40:05 +0100

    Full Pipeline Integration
    Added full data handling pipeline

--- MLWithSci-kit/Chapter2/HousingCostPrediction/HousingCostPredictor.py
+++ MLWithSci-kit/Chapter2/HousingCostPrediction/HousingCostPredictor.py
@@ -12,8 +12,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from zlib import crc32
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer,CategoricalEncoder,OrdinalEncoder,OneHotEncoder,StandardScaler
 from sklearn.impute import SimpleImputer
+from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.pipeline import Pipeline,FeatureUnion
+
 
 
 
@@ -72,6 +75,81 @@ housing_num = housing.drop("ocean_proximity",axis=1)
 imputer.fit(housing_num)
 X= imputer.transform(housing_num)
 housing_tr = pd.DataFrame(X,columns=housing_num.columns)
+
+
+#USing the Categorical encoder process to convert text to number to one.hots
+housing_cat = housing["ocean_proximity"]
+
+housing_cat_encoded, housing_categories = housing_cat.factorize()
+encoder = OneHotEncoder(categories='auto')
+housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1,1))
+
+
+#Creating my own pipeline
+
+rooms_ix,bedrooms_ix,population_ix,household_ix = 3,4,5,6
+
+class CombinedAttributesAdder(BaseEstimator,TransformerMixin):
+
+    def __init__(self,add_bedrooms_per_room = True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self,X,y=None):
+        return self
+
+    def transform(self,X,y=None):
+        rooms_per_household = X[:,rooms_ix]/X[:,household_ix]
+        population_per_household = X[:,population_ix] / X[:,household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:,bedrooms_ix]/X[:,rooms_ix]
+            return np.c_[X,rooms_per_household,population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X,rooms_per_household,population_per_household]
+
+class DataFrameSelector(BaseEstimator,TransformerMixin):
+    def __init__(self,attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X):
+        return X[self.attribute_names].values
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+
+#Now  we do transformation pipelines
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(missing_values=np.nan,strategy = "median")),
+    ('attribs_adder',CombinedAttributesAdder()),
+    ('std_scaler',StandardScaler())
+])
+
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+num_pipeline = Pipeline([
+    ('selector',DataFrameSelector(num_attribs)),
+    ('imputer', SimpleImputer(missing_values=np.nan,strategy = "median")),
+    ('attribs_adder',CombinedAttributesAdder()),
+    ('std_scaler',StandardScaler()),
+])
+
+cat_pipeline = Pipeline([
+    ('selector',DataFrameSelector(cat_attribs)),
+    ('cat_encoder',OneHotEncoder(categories='auto')),
+])
+
+full_pipeline = FeatureUnion(transformer_list=[
+    ("num_pipeline", num_pipeline),
+    ("cat_pipeline",cat_pipeline),
+])
+
+
+housing_prepared = full_pipeline.fit_transform(housing)
 
 
 #file = open("testfile.txt","w")
